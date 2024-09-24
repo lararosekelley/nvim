026d6bda9289a9745014a14ab287d4a97810939e
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2024-09-24 09:16:54 -0400

    tsserveer -> ts_ls

--- lua/user/plugins/lsp/handlers.lua
+++ lua/user/plugins/lsp/handlers.lua
@@ -78,7 +78,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	local disable_formatting_clients = { tsserver = true, lua_ls = true }
+	local disable_formatting_clients = { ts_ls = true, lua_ls = true }
 
 	if disable_formatting_clients[client.name] then
 		client.server_capabilities.document_formatting = false
