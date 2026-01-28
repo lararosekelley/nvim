9f0dafac13a368e87d15b4179390f61da23b662a
Lara Kelley <lararosekelley@gmail.com>
2026-01-28 13:25:34 -0500

    fix(rust): fix rust configuration

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -8,6 +8,27 @@ return {
     version = "^6",
     lazy = false,
     ft = { "rust" },
+    init = function()
+      local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/rust-analyzer"
+      vim.g.rustaceanvim = {
+        server = {
+          cmd = function()
+            if vim.fn.executable(mason_bin) == 1 then
+              return { mason_bin }
+            else
+              return { "rust-analyzer" }
+            end
+          end,
+          default_settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                command = "clippy",
+              },
+            },
+          },
+        },
+      }
+    end,
   },
   {
     "saecki/crates.nvim",
